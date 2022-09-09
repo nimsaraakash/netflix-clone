0301a58c4c9a70053973765cc807ff5f7466bd63
Nimsara Akash <72033383+nimsaraakash@users.noreply.github.com>
2022-09-09 16:43:44 +0530

    initial commit

---
+++ src/Nav.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import "./Nav.css"
+
+function Nav() {
+
+    const [show, handleShow] = useState(false);
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            } else handleShow(false);
+        });
+        return() => {
+            window.removeEventListener("scroll", null);
+        };
+    }, []);
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+        <img 
+        className='nav_logo'
+        src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png?20190206123158tps://commons.wikimedia.org/wiki/File:Netflix_2015_logo.svg'
+        alt='Netflix Logo'
+        />
+        <img 
+        className='nav_avatar'
+        src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
+        alt='Netflix Logo'
+        />
+    </div>
+  )
+}
+
+export default Nav;
